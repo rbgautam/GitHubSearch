ef5756462345fc8fa409af9e63876e76eac6bc7a
deltaman2000 <rahulbgautam@gmail.com>
2018-01-21 13:42:17 -0600

    Added progressbar

--- GithubSearch/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ GithubSearch/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -21,11 +21,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android.datafrominternet.utilities.NetworkUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.URL;
@@ -38,6 +44,13 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mSearchResultsTextView;
 
+    // TODO (12) Create a variable to store a reference to the error message TextView
+    private TextView mErrorMessageTextView;
+
+
+    // TODO (24) Create a ProgressBar variable to store a reference to the ProgressBar
+    private ProgressBar mDownloadRequestProgressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +60,11 @@ public class MainActivity extends AppCompatActivity {
 
         mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
+        // TODO (13) Get a reference to the error TextView using findViewById
+        mErrorMessageTextView = (TextView) findViewById(R.id.tv_error_message_display);
+
+        // TODO (25) Get a reference to the ProgressBar using findViewById
+        mDownloadRequestProgressBar = (ProgressBar) findViewById(R.id.pb_loading_indicator);
     }
 
     // TODO (2) Create a method called makeGithubSearchQuery
@@ -62,9 +80,40 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+    // TODO (14) Create a method called showJsonDataView to show the data and hide the error
+    private void showJsonDataView(String githubSearchResults){
+        StringBuilder strBuilder = new StringBuilder();
+        try {
+            JSONObject downloadData = new JSONObject(githubSearchResults);
+            JSONArray items = downloadData.getJSONArray("items");
 
+
+            for(int i=0; i< items.length();i++){
+
+                JSONObject itemObj = (JSONObject) items.get(i);
+                strBuilder.append(itemObj.getString("url") +"-"+itemObj.getString("description")+"\n\n");
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        mSearchResultsTextView.setText(strBuilder.toString());
+        mErrorMessageTextView.setVisibility(View.INVISIBLE);
+    }
+
+    // TODO (15) Create a method called showErrorMessage to show the error and hide the data
+    private void showErrorMessage(){
+        mErrorMessageTextView.setText(R.string.error_message);
+        mSearchResultsTextView.setVisibility(View.INVISIBLE);
+
+    }
     private class GithubQueryTask extends AsyncTask<URL,Void,String>{
 
+        @Override
+        protected void onPreExecute() {
+            mDownloadRequestProgressBar.setVisibility(View.VISIBLE);
+        }
         @Override
         protected String doInBackground(URL... params) {
             String downloadData =  new String();
@@ -73,15 +122,25 @@ public class MainActivity extends AppCompatActivity {
                 downloadData = NetworkUtils.getResponseFromHttpUrl(queryURL);
 
             } catch (IOException e) {
+
                 e.printStackTrace();
+                showErrorMessage();
             }
             return downloadData;
         }
 
         @Override
-        protected void onPostExecute(String s) {
-            if(s != null && !s.isEmpty())
-                mSearchResultsTextView.setText(s);
+        protected void onPostExecute(String githubSearchResults) {
+            // TODO (27) As soon as the loading is complete, hide the loading indicator
+            mDownloadRequestProgressBar.setVisibility(View.INVISIBLE);
+            if (githubSearchResults != null && !githubSearchResults.equals("")) {
+                // TODO (17) Call showJsonDataView if we have valid, non-null results
+                showJsonDataView(githubSearchResults);
+
+            }else{
+                showErrorMessage();
+            }
+            // TODO (16) Call showErrorMessage if the result is null in onPostExecute
         }
     }
 
