d834b8cec1f68e64aa50df5cc54151fc7f3ba6e7
rahulbgautam <rahulbgautam@gmail.com>
2018-04-09 11:54:27 -0500

    Handle click event Added Detail activity

--- app/src/main/java/com/example/android/sunshine/utilities/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/utilities/ForecastAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine.utilities;
 
+
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,90 +11,119 @@ import android.widget.TextView;
 import com.example.android.sunshine.R;
 
 /**
- * Created by Rahul B Gautam on 4/8/18.
+ * {@link ForecastAdapter} exposes a list of weather forecasts to a
+ * {@link android.support.v7.widget.RecyclerView}
  */
-// Within ForecastAdapter.java /////////////////////////////////////////////////////////////////
-// TODO (15) Add a class file called ForecastAdapter
-// TODO (22) Extend RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder>
-
-// TODO (23) Create a private string array called mWeatherData
-
-// TODO (47) Create the default constructor (we will pass in parameters in a later lesson)
-
-// TODO (16) Create a class within ForecastAdapter called ForecastAdapterViewHolder
-// TODO (17) Extend RecyclerView.ViewHolder
-
-
-// TODO (24) Override onCreateViewHolder
-// TODO (25) Within onCreateViewHolder, inflate the list item xml into a view
-// TODO (26) Within onCreateViewHolder, return a new ForecastAdapterViewHolder with the above view passed in as a parameter
-
-// TODO (29) Override getItemCount
-// TODO (30) Return 0 if mWeatherData is null, or the size of mWeatherData if it is not null
-
-
-// Within ForecastAdapter.java /////////////////////////////////////////////////////////////////
-
-public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder>{
-
-
+public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
 
     private String[] mWeatherData;
 
-    public ForecastAdapter() {
+    /*
+     * An on-click handler that we've defined to make it easy for an Activity to interface with
+     * our RecyclerView
+     */
+    private final ForecastAdapterOnClickHandler mClickHandler;
 
+    /**
+     * The interface that receives onClick messages.
+     */
+    public interface ForecastAdapterOnClickHandler {
+        void onClick(String weatherForDay);
     }
-// Within ForecastAdapterViewHolder ///////////////////////////////////////////////////////////
-// TODO (18) Create a public final TextView variable called mWeatherTextView
 
-    // TODO (19) Create a constructor for this class that accepts a View as a parameter
-// TODO (20) Call super(view) within the constructor for ForecastAdapterViewHolder
-// TODO (21) Using view.findViewById, get a reference to this layout's TextView and save it to mWeatherTextView
-// Within ForecastAdapterViewHolder ///////////////////////////////////////////////////////////
-    public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
+    /**
+     * Creates a ForecastAdapter.
+     *
+     * @param clickHandler The on-click handler for this adapter. This single handler is called
+     *                     when an item is clicked.
+     */
+    public ForecastAdapter(ForecastAdapterOnClickHandler clickHandler) {
+        mClickHandler = clickHandler;
+    }
 
+    /**
+     * Cache of the children views for a forecast list item.
+     */
+    public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public final TextView mWeatherTextView;
 
+        public ForecastAdapterViewHolder(View view) {
+            super(view);
+            mWeatherTextView = (TextView) view.findViewById(R.id.tv_weather_data);
+            view.setOnClickListener(this);
+        }
 
-
-        public ForecastAdapterViewHolder(View itemView) {
-            super(itemView);
-            this.mWeatherTextView = (TextView) itemView.findViewById(R.id.tv_weather_data);
+        /**
+         * This gets called by the child views during a click.
+         *
+         * @param v The View that was clicked
+         */
+        @Override
+        public void onClick(View v) {
+            int adapterPosition = getAdapterPosition();
+            String weatherForDay = mWeatherData[adapterPosition];
+            mClickHandler.onClick(weatherForDay);
         }
     }
+
+    /**
+     * This gets called when each new ViewHolder is created. This happens when the RecyclerView
+     * is laid out. Enough ViewHolders will be created to fill the screen and allow for scrolling.
+     *
+     * @param viewGroup The ViewGroup that these ViewHolders are contained within.
+     * @param viewType  If your RecyclerView has more than one type of item (which ours doesn't) you
+     *                  can use this viewType integer to provide a different layout. See
+     *                  {@link android.support.v7.widget.RecyclerView.Adapter#getItemViewType(int)}
+     *                  for more details.
+     * @return A new ForecastAdapterViewHolder that holds the View for each list item
+     */
     @Override
-    public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
+    public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
         int layoutIdForListItem = R.layout.forecast_list_item;
         LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
 
-        View view = inflater.inflate(layoutIdForListItem, parent, false);
-        ForecastAdapterViewHolder viewHolder = new ForecastAdapterViewHolder(view);
-
-        return viewHolder;
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        return new ForecastAdapterViewHolder(view);
     }
 
-    // TODO (27) Override onBindViewHolder
-    // TODO (28) Set the text of the TextView to the weather for this list item's position
-
+    /**
+     * OnBindViewHolder is called by the RecyclerView to display the data at the specified
+     * position. In this method, we update the contents of the ViewHolder to display the weather
+     * details for this particular position, using the "position" argument that is conveniently
+     * passed into us.
+     *
+     * @param forecastAdapterViewHolder The ViewHolder which should be updated to represent the
+     *                                  contents of the item at the given position in the data set.
+     * @param position                  The position of the item within the adapter's data set.
+     */
     @Override
-    public void onBindViewHolder(ForecastAdapterViewHolder holder, int position) {
+    public void onBindViewHolder(ForecastAdapterViewHolder forecastAdapterViewHolder, int position) {
         String weatherForThisDay = mWeatherData[position];
-        holder.mWeatherTextView.setText(weatherForThisDay);
-
+        forecastAdapterViewHolder.mWeatherTextView.setText(weatherForThisDay);
     }
 
+    /**
+     * This method simply returns the number of items to display. It is used behind the scenes
+     * to help layout our Views and for animations.
+     *
+     * @return The number of items available in our forecast
+     */
     @Override
     public int getItemCount() {
-        if(mWeatherData == null)
-            return 0;
-
+        if (null == mWeatherData) return 0;
         return mWeatherData.length;
     }
 
-    // TODO (31) Create a setWeatherData method that saves the weatherData to mWeatherData
-    // TODO (32) After you save mWeatherData, call notifyDataSetChanged
-    public void setWeatherData(String[] weatherData){
+    /**
+     * This method is used to set the weather forecast on a ForecastAdapter if we've already
+     * created one. This is handy when we get new data from the web but don't want to create a
+     * new ForecastAdapter to display it.
+     *
+     * @param weatherData The new weather data to be displayed.
+     */
+    public void setWeatherData(String[] weatherData) {
         mWeatherData = weatherData;
         notifyDataSetChanged();
     }
