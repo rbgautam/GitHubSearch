056ec09138786389492fa23892d10cea36346145
rahulbgautam <rahulbgautam@gmail.com>
2018-04-09 12:24:44 -0500

    new share menu and share data

--- SunshineApp/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ SunshineApp/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -16,6 +16,7 @@
 package com.example.android.sunshine;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -27,7 +28,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.utilities.ForecastAdapter;
@@ -121,8 +121,11 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
         // TODO (1) Create a new Activity called DetailActivity using Android Studio's wizard
         // TODO (2) Change the root layout of activity_detail.xml to a FrameLayout and remove unnecessary xml attributes
         // TODO (3) Remove the Toast and launch the DetailActivity using an explicit Intent
-        Toast.makeText(context, weatherForDay, Toast.LENGTH_SHORT)
-                .show();
+        //Toast.makeText(context, weatherForDay, Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(MainActivity.this,DetailActivity.class);
+        intent.putExtra(Intent.EXTRA_TEXT,weatherForDay);
+        startActivity(intent);
     }
 
     /**
