406929388c2c91666f5021d622ee9e1b87051af1
rahulbgautam <rahulbgautam@gmail.com>
2018-04-20 19:32:41 -0500

    Changed orentation

--- GithubSearch/app/src/main/java/com/example/android/datafrominternet/data/GitHubListItemAdapter.java
+++ GithubSearch/app/src/main/java/com/example/android/datafrominternet/data/GitHubListItemAdapter.java
@@ -1,6 +1,8 @@
 package com.example.android.datafrominternet.data;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -148,7 +150,43 @@ public class GitHubListItemAdapter extends RecyclerView.Adapter<GitHubListItemAd
         public void onClick(View view) {
             int adapterPosition = getAdapterPosition();
             //String weatherForDay = mWeatherData[adapterPosition];
-
+//            Item item = mGitHubData.getItems().get(adapterPosition);
+//            Log.i("onclick",item.getHtmlUrl());
         }
+
+
+    }
+
+    /**
+     * This method fires off an implicit Intent to open a webpage.
+     *
+     * @param url Url of webpage to open. Should start with http:// or https:// as that is the
+     *            scheme of the URI expected with this Intent according to the Common Intents page
+     */
+    private void openWebPage(String url) {
+        // COMPLETED (2) Use Uri.parse to parse the String into a Uri
+        /*
+         * We wanted to demonstrate the Uri.parse method because its usage occurs frequently. You
+         * could have just as easily passed in a Uri as the parameter of this method.
+         */
+        Uri webpage = Uri.parse(url);
+
+        // COMPLETED (3) Create an Intent with Intent.ACTION_VIEW and the webpage Uri as parameters
+        /*
+         * Here, we create the Intent with the action of ACTION_VIEW. This action allows the user
+         * to view particular content. In this case, our webpage URL.
+         */
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        // COMPLETED (4) Verify that this Intent can be launched and then call startActivity
+        /*
+         * This is a check we perform with every implicit Intent that we launch. In some cases,
+         * the device where this code is running might not have an Activity to perform the action
+         * with the data we've specified. Without this check, in those cases your app would crash.
+         */
+
+//        if (intent.resolveActivity(mContext.getPackageManager()) != null) {
+//             startActivity(intent);
+//        }
     }
 }
