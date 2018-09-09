a29905bcc1414f3b55cbaa8c44aa536667050afb
rahulbgautam <rahulbgautam@gmail.com>
2018-09-09 14:09:22 -0500

    Added new Tabs for top repos and top Devs Used LiveData and viewmodel to populate recyclerView

---
+++ app/src/main/java/com/forgeinnovations/android/githubelite/datamodel/GitHubSearch/License.java
@@ -0,0 +1,48 @@
+package com.forgeinnovations.android.githubelite.datamodel.GitHubSearch;
+
+import com.squareup.moshi.Json;
+
+public class License {
+
+    @Json(name = "key")
+    private String key;
+    @Json(name = "name")
+    private String name;
+    @Json(name = "spdx_id")
+    private String spdxId;
+    @Json(name = "url")
+    private String url;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpdxId() {
+        return spdxId;
+    }
+
+    public void setSpdxId(String spdxId) {
+        this.spdxId = spdxId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
